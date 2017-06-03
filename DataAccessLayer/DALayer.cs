94e188f351bf646aae9e030e4673d436cd11da85
rodrigo andregnette hill <roroandre93@hotmail.com>
2017-06-03 15:38:17 -0300

    En controlador de reportes se agrega get valores en base a idsensor y fecha

--- DataAccessLayer/DALayer.cs
+++ DataAccessLayer/DALayer.cs
@@ -289,24 +289,29 @@ namespace DataAccessLayer
         public List<Valores> GetValoresDeSensorConFecha(int idSensor, String fecha)
         {
             DateTime dt = Convert.ToDateTime(fecha);
+           
             using (Model.DBTSI1Entities db_context = new Model.DBTSI1Entities())
             {
                 List<Valores> valores = new List<Valores>();
                 var valor = (from v in db_context.Valores
                              where v.Id_Sensor == idSensor
-                             where v.Fecha == dt
                              select v);
+               
                 foreach (Model.Valores v in valor)
                 {
-                    Valores val = new Valores()
-                    {
-                        Id = v.Id,
-                        Id_Sensor = v.Id_Sensor,
-                        Valor_ = v.Valor,
-                        Fecha = v.Fecha
-                    };
 
-                    valores.Add(val);
+                    if (v.Fecha.Date == dt.Date)
+                    {
+                        Valores val = new Valores()
+                        {
+                            Id = v.Id,
+                            Id_Sensor = v.Id_Sensor,
+                            Valor_ = v.Valor,
+                            Fecha = v.Fecha
+                        };
+
+                        valores.Add(val);
+                    }
                 }
                 return valores;
             }
